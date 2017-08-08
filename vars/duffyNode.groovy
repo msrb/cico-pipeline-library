d122283013b36b00df2fd0d134b786011114f2cf
Michal Srb <michal@redhat.com>
2017-08-09 00:25:53 +0200

    17

--- vars/duffyNode.groovy
+++ vars/duffyNode.groovy
@@ -3,23 +3,13 @@ import hudson.model.*
 import hudson.slaves.*
 import hudson.plugins.sshslaves.*
 import java.util.ArrayList;
-
 import java.util.UUID;
+
 
 def call(Closure body) {
 
-    String nodeName = "duffy-" + UUID.randomUUID().toString();
+    final String nodeName = "duffy-" + UUID.randomUUID().toString();
 
-    //final Slave slave = new DumbSlave(
-    //                        nodeName,
-    //                        "Agent node description",
-    //                        "/home/jenkins",
-    //                        "1",
-    //                        Node.Mode.EXCLUSIVE,
-    //                        nodeName,
-    //                        new SSHLauncher("agenNode", 22, "user", "password", "", "", "", "", ""),
-    //                        new RetentionStrategy.Always(),
-    //                        new LinkedList())
     try {
 
         addNode(nodeName)
@@ -35,24 +25,20 @@ def call(Closure body) {
 
 @NonCPS
 def addNode(nodeName) {
+    Node node = new DumbSlave(
+                    nodeName,
+                    "Duffy node for " + env.JOB_NAME,
+                    "/home/root/",
+                    "1",
+                    Node.Mode.EXCLUSIVE,
+                    nodeName,
+                    new SSHLauncher("agenNode", 22, "user", "password", "", "", "", "", ""),
+                    new RetentionStrategy.Always(),
+                    new LinkedList())
     Jenkins.instance.addNode(getSlave(nodeName))
 }
 
 @NonCPS
 def removeNode(nodeName) {
-    Jenkins.instance.removeNode(Jenkins.instance.getNode(nodeName))
-}
-
-@NonCPS
-def getSlave(nodeName) {
-    return new DumbSlave(
-               nodeName,
-               "Agent node description",
-               "/home/jenkins",
-               "1",
-               Node.Mode.EXCLUSIVE,
-               nodeName,
-               new SSHLauncher("agenNode", 22, "user", "password", "", "", "", "", ""),
-               new RetentionStrategy.Always(),
-               new LinkedList())
+    Jenkins.instance.removeNode(Jenkins.instance.getNode('abc'))
 }
